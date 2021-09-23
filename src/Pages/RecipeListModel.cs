57a380e063d24401edd713b78c59a8d2eddd8793
danieliusre <danielius.rekus@example.com>
2021-09-23 14:46:21 +0300

    Working calculator update

--- src/Pages/RecipeListModel.cs
+++ src/Pages/RecipeListModel.cs
@@ -2,7 +2,6 @@ public class RecipeModel
     {
         public string Item {get;set;}
         public int Left {get;set;}
-
         public string Unit {get;set;}
         public string Date {get;set;}
     }
