e978703f61f1224a3d896b8002e2a6e071965f66
emasink <emmasi939@gmail.com>
2021-10-10 20:38:24 +0300

    Authorization on sign in

--- src/CustomAuthStateProvider.cs
+++ src/CustomAuthStateProvider.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Authorization;
+using Wasted.Data;
 
 namespace Wasted
 {
@@ -8,14 +9,20 @@ namespace Wasted
     {
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            var identity = new ClaimsIdentity(new[]
-            {
-                new Claim(ClaimTypes.Name, "mrfibuli"),
-            }, "Fake authentication type");
-
+            var identity = new ClaimsIdentity();
             var user = new ClaimsPrincipal(identity);
-
             return Task.FromResult(new AuthenticationState(user));
+        }
+        public void markUserAsAuthenticated(string emailAddress)
+        {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, emailAddress),
+                }, "Fake authentication type");
+
+                var user = new ClaimsPrincipal(identity);
+
+                NotifyAuthenticationStateChanged (Task.FromResult(new AuthenticationState(user)));
         }
     }
 }
