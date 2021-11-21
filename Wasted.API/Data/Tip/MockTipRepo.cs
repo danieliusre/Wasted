1cf6ec545061068350e0abea463d57640c43a3f9
Arvydasss <76534450+Arvydasss@users.noreply.github.com>
2021-11-21 18:48:47 +0200

    Tip API

---
+++ Wasted.API/Data/Tip/MockTipRepo.cs
@@ -0,0 +1,43 @@
+using Wasted.API.Models;
+using System.Collections.Generic;
+
+namespace Wasted.API.Data
+{
+    public class MockTipRepo : ITipRepo
+    {
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+        public IEnumerable<Tip> GetTipList()
+        {
+            List<Tip> Tips = new List<Tip>
+            {
+                new Tip { TipId = 1, TipName = "Shop Smart", Name = "To avoid buying more food than you need, make frequent trips to the grocery store every few days rather than doing a bulk shopping trip once a week.", TipLikes = 4, TipDislikes = 0, Link = "https://en.wikipedia.org/wiki/Smart_shop"},
+                new Tip { TipId = 2, TipName = "Store Food Correctly", Name = "Separating foods that produce more ethylene gas from those that don’t is another great way to reduce food spoilage. Ethylene promotes ripening in foods and could lead to spoilage.", TipLikes = 4, TipDislikes = 0, Link = "https://www.betterhealth.vic.gov.au/health/healthyliving/food-safety-and-storage"},
+                new Tip { TipId = 3, TipName = "Learn to Preserve", Name = "Pickling, drying, canning, fermenting, freezing and curing are all methods you can use to make food last longer, thus reducing waste.", TipLikes = 1, TipDislikes = 0, Link = "https://www.masterclass.com/articles/a-guide-to-home-food-preservation-how-to-pickle-can-ferment-dry-and-preserve-at-home"}
+            };
+            return Tips;
+        }
+
+        public Tip GetTipById(int id)
+        {
+            return new Tip { TipId = 2, TipName = "Store Food Correctly", Name = "Separating foods that produce more ethylene gas from those that don’t is another great way to reduce food spoilage. Ethylene promotes ripening in foods and could lead to spoilage.", TipLikes = 4, TipDislikes = 0, Link = "https://www.betterhealth.vic.gov.au/health/healthyliving/food-safety-and-storage"};
+        }
+
+        public void CreateNewTip(Tip tip)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateTip(Tip tip)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteTip(Tip tip)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Wasted.API/Dtos/Tip/TipReadDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Wasted.API.Dtos
+{
+    public class TipReadDto
+    {
+        public int TipId { get; set; }
+        public string TipName { get; set; }
+        public string Name { get; set; }
+        public int TipLikes { get; set; }
+        public int TipDislikes { get; set; }
+        public string Link { get; set; }
+    }
+}
