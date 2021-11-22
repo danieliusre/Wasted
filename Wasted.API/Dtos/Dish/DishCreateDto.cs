a4a0efda0bd7d7c1a7e66dae253590dc777ad7aa
Andrius <andriukas.tumsys@gmail.com>
2021-11-22 15:49:21 +0200

    Fix

--- Wasted.API/Dtos/Dish/DishCreateDto.cs
+++ Wasted.API/Dtos/Dish/DishCreateDto.cs
@@ -10,10 +10,18 @@ namespace Wasted.API.Dtos
         public string Name { get; set; }
 
         [Required]
+<<<<<<< HEAD
         public int numberOfIngredients { get; set; }
 
         [Required]
         [MaxLength(500)]
+=======
+        [MaxLength(3)]
+        public int numberOfIngredients { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+>>>>>>> parent of cc1a163 (Revert "Dish API")
         public string Ingredients { get; set; }
 
         [Required]
